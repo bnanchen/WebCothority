3285c80babe24e79cd5efa17372d90979ebd0b59
bnanchen <nanchenbastian@gmail.com>
2016-11-23 20:47:23 +0100

    Signature well done

--- scripts/sign.js
+++ scripts/sign.js
@@ -1,7 +1,14 @@
 // Signing part
 function sign(fileToSign) {
+    // instantiate the nacl module:
     nacl_factory.instantiate(function(nacl) {
-        var sha256File = nacl.crypto_hash_sha256(bytesToHex(fileToSign));
-        console.log(sha256File);
+        var sha256File = nacl.crypto_hash_sha256(bytesToHex(fileToSign)); // Uint8Array
+        var keys = nacl.crypto_sign_keypair(); // return: {signPk, signSk} with signPk public key and signSk private key
+        var signedFile = nacl.crypto_sign(sha256File, keys.signSk); // Uint8Array
+        if (nacl.crypto_sign_open(signedFile, keys.signPk) == null) {
+            console.log("arf");
+        } else {
+            console.log("Good signature");
+        }
     });
 }
