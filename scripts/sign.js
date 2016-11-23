0d41e99a3fd439c39a96cbc3d21c6a65ae0972f1
bnanchen <nanchenbastian@gmail.com>
2016-11-23 20:52:24 +0100

    translation of the signature to base64

--- scripts/sign.js
+++ scripts/sign.js
@@ -6,9 +6,12 @@ function sign(fileToSign) {
         var keys = nacl.crypto_sign_keypair(); // return: {signPk, signSk} with signPk public key and signSk private key
         var signedFile = nacl.crypto_sign(sha256File, keys.signSk); // Uint8Array
         if (nacl.crypto_sign_open(signedFile, keys.signPk) == null) {
-            console.log("arf");
+            console.log("arf"); // signature is not good
         } else {
             console.log("Good signature");
         }
+        // translate the signature in base64:
+        var signedFilebase64 = btoa(String.fromCharCode.apply(null, signedFile));
+        alert(signedFilebase64);
     });
 }
